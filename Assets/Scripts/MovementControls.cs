683318f584f4e1471bb37eebea06df6b4dbc5e76
Carter.bourne <carter.bourne@chatham.edu>
2020-02-13 16:21:24 -0500

    Added Instantiate code
    Instantiate code for Object Pooler

--- Assets/Scripts/MovementControls.cs
+++ Assets/Scripts/MovementControls.cs
@@ -4,64 +4,62 @@ using UnityEngine;
 using DylanMovement;
 
 [RequireComponent(typeof(DylanMovement.BasicMovement))]
-    public class MovementControls : MonoBehaviour
-        {
-             /*Dylan Boswell
-            * To be placed on the player character gameobject
-            * requires BasicMovement.cs to function properly*/
-            DylanMovement.BasicMovement controller;
+public class MovementControls : MonoBehaviour
+{
+    /*Dylan Boswell
+   * To be placed on the player character gameobject
+   * requires BasicMovement.cs to function properly*/
+    DylanMovement.BasicMovement controller;
 
     public GameObject bulletRevolver;
     public SpriteRenderer bulletSprite;
-           
-    
 
-            // Start is called before the first frame update
-            void Start()
-                {
-        
-                }
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject bullet = GameObject.Instantiate(bulletRevolver, new Vector3(1, 1, 0), Quaternion.identity);
+    }
 
-            public void Awake()
-                {
-                    controller = GetComponent<DylanMovement.BasicMovement>();
-                    /*assigns controller on waking*/
-                }
+    public void Awake()
+    {
+        controller = GetComponent<DylanMovement.BasicMovement>();
+        /*assigns controller on waking*/
+    }
 
-            // Update is called once per frame
-            public void Update()
-                {
-                    
-                    if(controller == null)
-                     {
-                            Debug.LogError("controller ain't workin my dude");
-                            /*error if there is no controller assigned, i.e., there is no BasicMovement.cs*/
-                     }
+    // Update is called once per frame
+    public void Update()
+    {
+
+        if (controller == null)
+        {
+            Debug.LogError("controller ain't workin my dude");
+            /*error if there is no controller assigned, i.e., there is no BasicMovement.cs*/
+        }
 
 
-                    controller.Move(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
-                    /*Get the orientation of the vertical and horizontal input axes every frame, supplies them to Move() from BasicMovement*/
- 
-                    if ((Input.GetButtonDown("Jump"))) /*activate if the spacebar is pressed*/
-                        
-                        {
-                            
+        controller.Move(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+        /*Get the orientation of the vertical and horizontal input axes every frame, supplies them to Move() from BasicMovement*/
 
-                            Debug.Log("Sneak!"); /*just a fun little message that tells you the command went through */
-                            controller.Sneak(); /*run sneak from BasicMovement*/
+        if ((Input.GetButtonDown("Jump"))) /*activate if the spacebar is pressed*/
 
-                
-                        }  
-
-                        if(Input.GetButtonDown("Fire1"))
-                        {
-                            Debug.Log("fire!");
-                            
-                        }
-               
-                }
+        {
 
 
-    
+            Debug.Log("Sneak!"); /*just a fun little message that tells you the command went through */
+            controller.Sneak(); /*run sneak from BasicMovement*/
 
-            }
+
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("fire!");
+
+        }
+
+    }
+
+
+
+
+}
